3b028ef296d92e000bfcfa9f9c4da3999e67e837
ratno putro sulistiyono <ratno@knoqdown.com>
2018-11-29 18:44:50 +0700

    helpers.php: fungsi uniform_tab untuk implement tab yang sama pada semua item array/line

--- src/helpers.php
+++ src/helpers.php
@@ -25,3 +25,18 @@ if(!function_exists("tab")) {
         }
     }
 }
+
+if(!function_exists("uniform_tab")) {
+    function uniform_tab($array, $uniform_tab_number)
+    {
+        $out = [];
+        $idx = 0;
+        if(is_array($array)) {
+            foreach($array as $line) {
+                $out[] = tab($uniform_tab_number,$idx++) . $line;
+            }
+        }
+
+        return implode("\n",$out);
+    }
+}
