3f490f1178e7988f8346636099ce5ee2bbcc0fb2
ratno putro sulistiyono <ratno@knoqdown.com>
2018-11-30 09:42:43 +0700

    Column.php: index uuid unique

--- src/Column.php
+++ src/Column.php
@@ -182,7 +182,7 @@ class Column
     public function uuid() : Column
     {
         $this->column_uuid = true;
-        return $this->char(36)->required()->grid_hide()->form_hide();
+        return $this->char(36)->required()->unique()->grid_hide()->form_hide();
     }
 
     public function unsigned() : Column
